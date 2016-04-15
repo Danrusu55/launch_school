4f041c86a8087c62d49cb2b33f221c1329a1c0f4
alexi21 <alexi2@protonmail.com>
2016-04-16 00:53:32 +1000

    Ran rubocop on tic_tac_toe.rb (7 offenses still detected but...happy with code)

--- programming_foundations/lesson_4/tic_tac_toe.rb
+++ programming_foundations/lesson_4/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 # tic_tac_toe.rb
 require 'pry'
 
-board_hash = {a1: ' ', a2: ' ', a3: ' ', b1: ' ', b2: ' ', b3: ' ', c1: ' ', c2: ' ', c3: ' '}
+board_hash = { a1: ' ', a2: ' ', a3: ' ', b1: ' ', b2: ' ', b3: ' ', c1: ' ', c2: ' ', c3: ' ' }
 
 def build_board(board_hash)
   puts "\n"
@@ -18,8 +18,8 @@ end
 # check that the selected key is not occupied
 # if meets both conditions return true, otherwise return false
 def check_input_valid(col_row, board_hash)
-  board_hash.each do |k,v|
-    if (col_row.to_sym == k && board_hash[col_row.to_sym] == ' ')
+  board_hash.each do |k, _|
+    if col_row.to_sym == k && board_hash[col_row.to_sym] == ' '
       return true
     end
   end
@@ -28,51 +28,50 @@ end
 
 def check_winner(board_hash, player)
   (board_hash[:a1] == player && board_hash[:a2] == player && board_hash[:a3] == player) ||
-  (board_hash[:b1] == player && board_hash[:b2] == player && board_hash[:b3] == player) ||
-  (board_hash[:c1] == player && board_hash[:c2] == player && board_hash[:c3] == player) ||
-  (board_hash[:a1] == player && board_hash[:b1] == player && board_hash[:c1] == player) ||
-  (board_hash[:a2] == player && board_hash[:b2] == player && board_hash[:c2] == player) ||
-  (board_hash[:a3] == player && board_hash[:b3] == player && board_hash[:c3] == player) ||
-  (board_hash[:a1] == player && board_hash[:b2] == player && board_hash[:c3] == player) ||
-  (board_hash[:a3] == player && board_hash[:b2] == player && board_hash[:c1] == player)
+    (board_hash[:b1] == player && board_hash[:b2] == player && board_hash[:b3] == player) ||
+    (board_hash[:c1] == player && board_hash[:c2] == player && board_hash[:c3] == player) ||
+    (board_hash[:a1] == player && board_hash[:b1] == player && board_hash[:c1] == player) ||
+    (board_hash[:a2] == player && board_hash[:b2] == player && board_hash[:c2] == player) ||
+    (board_hash[:a3] == player && board_hash[:b3] == player && board_hash[:c3] == player) ||
+    (board_hash[:a1] == player && board_hash[:b2] == player && board_hash[:c3] == player) ||
+    (board_hash[:a3] == player && board_hash[:b2] == player && board_hash[:c1] == player)
 end
 
 def check_draw(board_hash)
-  board_hash.each do |k,v|
+  board_hash.each do |_, v|
     return false if v == ' '
   end
   true
 end
 
-
 def computers_turn
   computers_choice = rand(1..9)
   case computers_choice
-    when 1
-      return 'a1'
-    when 2
-      return 'a2'
-    when 3
-      return 'a3'
-    when 4
-      return 'b1'
-    when 5
-      return 'b2'
-    when 6
-      return 'b3'
-    when 7
-      return 'c1'
-    when 8
-      return 'c2'
-    when 9
-      return 'c3'
+  when 1
+    return 'a1'
+  when 2
+    return 'a2'
+  when 3
+    return 'a3'
+  when 4
+    return 'b1'
+  when 5
+    return 'b2'
+  when 6
+    return 'b3'
+  when 7
+    return 'c1'
+  when 8
+    return 'c2'
+  when 9
+    return 'c3'
   end
 end
 
 # LOOP here
 loop do
   # Build empty board
-  board_hash = {a1: ' ', a2: ' ', a3: ' ', b1: ' ', b2: ' ', b3: ' ', c1: ' ', c2: ' ', c3: ' '}
+  board_hash = { a1: ' ', a2: ' ', a3: ' ', b1: ' ', b2: ' ', b3: ' ', c1: ' ', c2: ' ', c3: ' ' }
   col_row = ' '
 
   # LOOP here
@@ -86,7 +85,7 @@ loop do
     loop do
       col_row = gets.chomp
 
-      # Check input is valid 
+      # Check input is valid
       break if check_input_valid(col_row, board_hash)
       puts "Please make a valid choice: "
     end
@@ -109,7 +108,7 @@ loop do
       puts "It is a draw . . ."
       break
     end
-    
+
     # Player two has turn
     puts "Computer's turn . . ."
     loop do
@@ -127,15 +126,13 @@ loop do
       puts "computer won!!"
       break
     end
-
   end
   # END LOOP here
 
-# Check if player wants another game
-# If not break
-puts "Would you like another game (y - yes)?"
-play_again = gets.chomp
-break if play_again[0].downcase != 'y'
-
+  # Check if player wants another game
+  # If not break
+  puts "Would you like another game (y - yes)?"
+  play_again = gets.chomp
+  break if play_again[0].downcase != 'y'
 end
 # END LOOP here
